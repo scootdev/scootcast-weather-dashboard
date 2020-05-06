736602c131bfa3daf97355d22b4e22b3dc32b677
Scott Harris <scottsharris@yahoo.com>
2020-05-06 14:05:58 -0700

    added functions to retrieve and display weather data for searched location

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var cityList = [];
-var results;
 
-function citySearch(result) {
+function citySearch(result, city) {
     $.ajax({
-        url: "api.openweathermap.org/data/2.5/weather?lat=" + result.lat + "&lon=" + result.lng + "&appid=c527141d78b015be3cd3681e7bc25ea1",
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + result.lat + "&lon=" + result.lng + "&units=imperial&appid=c527141d78b015be3cd3681e7bc25ea1",
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        displayResult(response, city);
+
     })
+}
+
+function displayResult(result, city) {
+    $("#city").text(city + " (" + moment().format('l') + ")");
+    var icon = $("<img src='https://openweathermap.org/img/wn/" + result.current.weather[0].icon + "@2x.png'>")
+    $("#city").append(icon);
+    $("#temp").text("Temperature: " + result.current.temp + " °F");
+    $("#humidity").text("Humidity: " + result.current.humidity + "%");
+    $("#wind").text("Wind Speed: " + result.current.wind_speed + " MPH");
+    $("#uv").text("UV Index: " + result.current.humidity + "%");
+
 }
 
 // Convert address into coordinates for accurate results
@@ -16,9 +28,24 @@ function geocodeAddress(address) {
         url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDnRG3kLW44MTYI0s7fplt8aQMFvRe1glQ",
         method: "GET"
     }).then(function (response) {
-        result = response.results[0].geometry.location;
-        citySearch(result);
+        console.log(response);
+        var city = response.results[0].formatted_address;
+        var result = response.results[0].geometry.location;
+        cityList.push(city);
+        
+        renderCityList();
+        citySearch(result, city);
     })
+}
+
+function renderCityList() {
+    list = $("#saved-searches");
+    list.html("")
+    for (var i = 0; i < cityList.length; i++) {
+        var listEl = $("<li class='list-group-item'></li>")
+        listEl.text(cityList[i]);
+        list.append(listEl);
+    }
 }
 
 // Using Google Maps API to autocomplete the search bar
